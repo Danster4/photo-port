61913d721c14b63875166126fbb23a285b153aeb
Danster4 <86613317+Danster4@users.noreply.github.com>
2022-01-04 10:31:10 -0500

    delete old psuedocode

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -12,23 +12,6 @@ const Nav = () => {
   const handleClick = (name) => {
     console.log(`${name} clicked`)
   }
-  // const categories = [
-  //   {
-  //     name: "commercial",
-  //     description:
-  //       "Photos of grocery stores, food trucks, and other commercial projects",
-  //   },
-  //   { name: "portraits", description: "Portraits of people in my life" },
-  //   { name: "food", description: "Delicious delicacies" },
-  //   {
-  //     name: "landscape",
-  //     description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-  //   },
-  // ];
-
-  // function categorySelected() {
-  //   console.log(`${name} clicked`)
-  // }
 
   return (
     <header data-testid="header" className="flex-row px-1">
